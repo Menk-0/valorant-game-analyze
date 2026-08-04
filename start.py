0b0020a73d1d4ff3ad44c0ab515def2c006ceec7
Menk-0 <107355314+Menk-0@users.noreply.github.com>
2026-08-05 00:43:07 +0530

    initial commit

---
+++ start.py
@@ -0,0 +1,6 @@
+def multiplication(a,b):
+    return(value_b*value_a)
+value_a = int(input("Enter value a"))
+value_b= int(input("Enter value B"))
+answer=multiplication(value_a,value_b)
+print(answer)
