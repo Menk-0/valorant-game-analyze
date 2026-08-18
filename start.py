c10bc48221ce77d07a810a99168c2647be57eb3e
Menk-0 <107355314+Menk-0@users.noreply.github.com>
2026-08-18 11:41:06 +0530

    added region var

--- start.py
+++ start.py
@@ -10,3 +10,8 @@ if len(taglineonly)>=3 and len(taglineonly)<=5:
 else:
     print("Please enter a valid tagline")
     quit()
+region_of_player=input("Enter your region only in the format of na, eu, ap, kr- ").lower()
+validregion=["kr","ap","na", "eu"]
+if (region_of_player not in validregion):
+    print("Enter a valid region")
+    quit()
