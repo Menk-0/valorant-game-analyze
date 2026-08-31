b238d50e5a2e7d5bc4a0f43dbbbff23ec1d72d5d
Menk-0 <107355314+Menk-0@users.noreply.github.com>
2026-08-31 20:25:57 +0530

    opeaning file

--- json_work/analyzer.py
+++ json_work/analyzer.py
@@ -1,3 +1,9 @@
 import json
 import os
 import pandas as pd
+
+with open("json_files/playerseparated.json") as file:
+    my_matches=json.load(file)
+
+with open("json_files/otherplayerseparated.json") as file2:
+    other_mathes=json.load(file2)
